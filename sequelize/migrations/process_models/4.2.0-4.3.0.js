c7eda2ef20ae9bdb5b0f9f300ffdc71e642a9c62
ElRaptorus <christian.werner@5minds.de>
2018-08-31 17:53:42 +0200

    :recycle: Refactor migrations to support multiple storages

---
+++ sequelize/migrations/process_models/4.2.0-4.3.0.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const Logger = require('loggerhythm').Logger;
+
+const logger = Logger.createLogger('process-engine:migration:sequelize');
+
+// See manual:
+// https://sequelize.readthedocs.io/en/latest/docs/migrations/#functions
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    logger.info('No migrations for process models storage necessary');
+  },
+  down: async (queryInterface, Sequelize) => {
+    logger.info('Running reverting migrations');
+    return Promise.resolve();
+  },
+};
