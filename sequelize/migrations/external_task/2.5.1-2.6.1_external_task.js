140ddb85c2774330eb1a47c2bc0189bd676486cd
Robin <robin.lenz@5minds.de>
2018-10-21 00:31:52 +0200

    :sparkles: migration for external task version column added

---
+++ sequelize/migrations/external_task/2.5.1-2.6.1_external_task.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// See manual:
+// https://sequelize.readthedocs.io/en/latest/docs/migrations/#functions
+
+// CHANGE NOTES:
+// Some of the earlier versions of the migrations apparently produced an unnamed foreign key on ProcessToken.
+// These cannot be addressed during migration and will potentially break them, when using sqlite.
+// We must therefore rebuild the ProcessTokenTable in order to remove that unnamed foreign key.
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+
+        // New Column for ExternalTasks
+        await queryInterface.addColumn(
+            'ExternalTasks',
+            'version',
+            {
+                type: Sequelize.INTEGER,
+                allowNull: true,
+            }
+        );
+    },
+    down: async (queryInterface, Sequelize) => {
+        console.log('Running reverting migrations');
+        return Promise.resolve();
+    },
+};
