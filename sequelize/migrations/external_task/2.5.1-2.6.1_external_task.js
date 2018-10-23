cf32972f2b2bf4ab1693fd9054fbdcf69b26634e
Robin <robin.lenz@5minds.de>
2018-10-23 10:17:38 +0200

    :recycle: changes requested in pr

--- sequelize/migrations/external_task/2.5.1-2.6.1_external_task.js
+++ sequelize/migrations/external_task/2.5.1-2.6.1_external_task.js
@@ -4,24 +4,22 @@
 // https://sequelize.readthedocs.io/en/latest/docs/migrations/#functions
 
 // CHANGE NOTES:
-// Some of the earlier versions of the migrations apparently produced an unnamed foreign key on ProcessToken.
-// These cannot be addressed during migration and will potentially break them, when using sqlite.
-// We must therefore rebuild the ProcessTokenTable in order to remove that unnamed foreign key.
+// Added the "version" column.
 module.exports = {
-    up: async (queryInterface, Sequelize) => {
+  up: async (queryInterface, Sequelize) => {
 
-        // New Column for ExternalTasks
-        await queryInterface.addColumn(
-            'ExternalTasks',
-            'version',
-            {
-                type: Sequelize.INTEGER,
-                allowNull: true,
-            }
-        );
-    },
-    down: async (queryInterface, Sequelize) => {
-        console.log('Running reverting migrations');
-        return Promise.resolve();
-    },
+    // New Column for ExternalTasks
+    await queryInterface.addColumn(
+      'ExternalTasks',
+      'version',
+      {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      }
+    );
+  },
+  down: async (queryInterface, Sequelize) => {
+    console.log('Running reverting migrations');
+    return Promise.resolve();
+  },
 };
