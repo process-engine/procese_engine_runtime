1b2b6fcfd9cea0d6e13f1b1c1884b71557079253
robpal94 <robin.palkovits@5minds.de>
2018-11-02 13:58:37 +0100

    :white_check_mark: add name field assertion

--- test/3_consumer_api/user_tasks/get_correlation_process_model_user_tasks.js
+++ test/3_consumer_api/user_tasks/get_correlation_process_model_user_tasks.js
@@ -62,9 +62,13 @@ describe(`Consumer API: ${testCase}`, () => {
     const userTask = userTaskList.userTasks[0];
 
     should(userTask).have.property('id');
+    should(userTask).have.property('name');
     should(userTask).have.property('correlationId');
     should(userTask).have.property('processModelId');
     should(userTask).have.property('data');
+
+    const sampleUserTaskName = 'Sample UserTask';
+    should(userTask.name).be.eql(sampleUserTaskName);
 
     should(userTask.data).have.property('formFields');
     should(userTask.data.formFields).be.instanceOf(Array);
