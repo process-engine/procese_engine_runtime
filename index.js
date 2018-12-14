78ac03c0eb33c52a8bdee5fa90d86eb0ffed3f69
ElRaptorus <christian.werner@5minds.de>
2018-12-14 10:58:43 +0100

    :recycle: Override the global promise with bluebird

--- index.js
+++ index.js
@@ -2,10 +2,13 @@
 'use strict';
 
 const InvocationContainer = require('addict-ioc').InvocationContainer;
+const Bluebird = require('bluebird');
 const fs = require('fs');
 const Logger = require('loggerhythm').Logger;
 const path = require('path');
 const platformFolders = require('platform-folders');
+
+global.Promise = Bluebird;
 
 const executeMigrations = require('./migrator').migrate;
 
