812af3bbdacc5b921e4a03c7f9f6d117d96310c1
robpal94 <robin.palkovits@5minds.de>
2019-01-18 10:01:44 +0100

    :sparkles: Add a Response for the Root Route

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const fs = require('fs');
 const Logger = require('loggerhythm').Logger;
 const path = require('path');
 const platformFolders = require('platform-folders');
+const packageJson = require('./package.json');
 
 Bluebird.config({
   cancellation: true,
@@ -74,6 +75,27 @@ async function startProcessEngine(sqlitePath) {
     await bootstrapper.start();
 
     logger.info('Bootstrapper started successfully.');
+
+    const httpExtension = await container.resolveAsync('HttpExtension');
+    httpExtension.app.get('/', (request, response) => {
+      const name = packageJson.name;
+      const version = packageJson.version;
+      const license = packageJson.license;
+      const contributors = packageJson.contributors;
+
+      const returnObject = {
+        name: name,
+        version: version,
+        license: license,
+        contributors: contributors,
+      };
+
+      response
+        .status(200)
+        .header('Content-Type', 'application/json')
+        .send(JSON.stringify(returnObject, null, 2));
+    });
+
   } catch (error) {
     logger.error('Bootstrapper failed to start.', error);
   }
