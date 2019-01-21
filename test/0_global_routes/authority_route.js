13d2aec7a7672105eabfd73a3ffa6210603e10d5
ElRaptorus <christian.werner@5minds.de>
2019-01-21 10:37:54 +0100

    :white_check_mark: Add tests for accessing global routes

---
+++ test/0_global_routes/authority_route.js
@@ -0,0 +1,75 @@
+'use strict';
+
+const should = require('should');
+const TestFixtureProvider = require('../../dist/commonjs').TestFixtureProvider;
+
+const HttpClient = require('@essential-projects/http').HttpClient;
+
+describe.only('Authority Route /security/authority - ', () => {
+
+  let httpClient;
+
+  let testFixtureProvider;
+
+  const routeToCall = 'security/authority';
+
+  before(async () => {
+    testFixtureProvider = new TestFixtureProvider();
+    await testFixtureProvider.initializeAndStart();
+
+    httpClient = new HttpClient();
+    httpClient.config = {
+      url: 'http://localhost:8000',
+    };
+  });
+
+  after(async () => {
+    await testFixtureProvider.tearDown();
+  });
+
+  it('Should return the address of the used authority, when an auth token is provided', async () => {
+
+    const requestHeaders = {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: testFixtureProvider.identities.defaultUser,
+      },
+    };
+
+    try {
+      const response = await httpClient.get(routeToCall, requestHeaders);
+      assertResponse(response);
+    } catch (error) {
+      should.fail(error, 'success', `Failed to run the request: ${error.message}`);
+    }
+  });
+
+  it('Should return the address of the used authority, even when no auth token is provided', async () => {
+
+    const requestHeaders = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    try {
+      const response = await httpClient.get(routeToCall, requestHeaders);
+      assertResponse(response);
+    } catch (error) {
+      should.fail(error, 'success', `Failed to run the request: ${error.message}`);
+    }
+  });
+
+  function assertResponse(response) {
+
+    should(response.status).be.equal(200);
+
+    const actualResult = response.result;
+
+    const expectedResult = {
+      authority: 'http://localhost:5000/',
+    };
+
+    should(actualResult).be.eql(expectedResult);
+  }
+});
