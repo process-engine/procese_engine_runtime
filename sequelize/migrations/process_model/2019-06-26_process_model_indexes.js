0f02ce850b520fa59e5d6b64471fbe20497237f8
ElRaptorus <christian.werner@5minds.de>
2019-07-01 09:44:07 +0200

    :recycle: Change column type of ProcessDefinition.hash to string
    MySql cannot use TEXT type columns as index.
    https://stackoverflow.com/questions/1827063/mysql-error-key-specification-without-a-key-length
    Using standard VARCHAR(255) - which is implied by the use of STRING - should suffice here,
    since the hashes stored in that column have a length of 60.
    So changing the type should be safe.

--- sequelize/migrations/process_model/2019-06-26_process_model_indexes.js
+++ sequelize/migrations/process_model/2019-06-26_process_model_indexes.js
@@ -10,6 +10,22 @@ module.exports = {
 
     console.log('Adding indexes to ProcessDefinitions table for each commonly used query operation');
 
+    // NOTE: MySql cannot use TEXT type columns as index.
+    // https://stackoverflow.com/questions/1827063/mysql-error-key-specification-without-a-key-length
+    // Using standard VARCHAR(255) - which is implied by the use of STRING - should suffice here,
+    // since the hashes stored in that column have a length of 60.
+    // So changing the type should be safe.
+    await queryInterface.changeColumn(
+      'ProcessDefinitions',
+      'hash',
+      {
+        type: Sequelize.STRING,
+        allowNull: false,
+        defaultValue: '',
+      }
+    );
+
+
     await queryInterface.addIndex('ProcessDefinitions', ['name']);
     await queryInterface.addIndex('ProcessDefinitions', ['hash']);
   },
