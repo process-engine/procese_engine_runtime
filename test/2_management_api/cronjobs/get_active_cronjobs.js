ec97ef5e8a76844f85964b576615702747b5bbda
Alex😎 <alexander.kasten@5minds.de>
2019-09-16 14:08:45 +0200

    :ok_hand: Add requested changes

--- test/2_management_api/cronjobs/get_active_cronjobs.js
+++ test/2_management_api/cronjobs/get_active_cronjobs.js
@@ -1,4 +1,4 @@
-
+'use strict';
 
 const should = require('should');
 
--- test/2_management_api/notifications/cronjob_updated_notifications.js
+++ test/2_management_api/notifications/cronjob_updated_notifications.js
@@ -1,5 +1,4 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-/* eslint-disable @typescript-eslint/no-require-imports */
+'use strict';
 
 const should = require('should');
 
