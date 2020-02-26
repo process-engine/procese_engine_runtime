1ba8743586f975f9186f587fe0387e4beefac464
ElRaptorus <christian.werner@5minds.de>
2020-02-26 10:22:04 +0100

    :recycle: Trim the response send through the new info route

--- src/modules/global_route_configurator.ts
+++ src/modules/global_route_configurator.ts
@@ -56,10 +56,16 @@ function configureRootRoute(useHttpRootRoutes: boolean): void {
 
   // This is meant as a compatibility route to ensure a consumer api client can communicate with the ProcessEngine and the AtlasEngine Proxy.
   httpExtension.app.get('/api/v1/consumer_api/info', (request: Request, response: Response): void => {
+
+    const trimmedResponse = {
+      name: packageInfo.name,
+      version: packageInfo.version,
+    };
+
     response
       .status(httpStatusCodeSuccess)
       .header('Content-Type', 'application/json')
-      .send(formattedResponse);
+      .send(trimmedResponse);
   });
 }
 
